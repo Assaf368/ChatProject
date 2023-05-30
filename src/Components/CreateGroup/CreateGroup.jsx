e524516342fb60b6e1624b88c76cff9f5bad700c
Assaf368 <assaf3434@gmail.com>
2023-05-30 15:17:13 +0300

    Re-designing CreateGroup component, changing, bgc image for chat

--- src/Components/CreateGroup/CreateGroup.jsx
+++ src/Components/CreateGroup/CreateGroup.jsx
@@ -1,18 +1,23 @@
 import { ImageUpload } from 'UiKit/Layouts/Elements/ImageUpload/ImageUpload'
 import './CreateGroup.css'
+import { Input } from 'UiKit/Layouts/Elements/Input/Input'
+import { useDispatch } from 'react-redux'
+import {SwichPickFriendsState  } from "State/toggle";
+
 
 export const CreateGroup = ({onSubmit,SetImage})=>{
+    const dispatch = useDispatch();
     return(
         <div className='create-group-container'>
             <div className='create-group-exit-container'>
-                <img className='exit-btn-create-group' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReBKjYJhzB-lApuM67GyvTwhk6SFbRxHC70w&usqp=CAU" alt="exit btn" />
+            <img onClick={()=> dispatch(SwichPickFriendsState())} className="create-group-exit-btn" src="/chat-exitBtn.png" alt="" />
             </div>
             <div className='create-group-header'>Create a group!</div>
             <div className='group-name-container'>
-                <input id='group-name-input' className='create-group-inputs' type="text" placeholder='Group name...' />
+                <Input title="Name" id='group-name-input' className='create-group-inputs' type="text" placeholder='Chose your group name...' />
             </div>
             <div className='discription-container'>
-                <input id='discription-input' className='create-group-inputs' type="text" placeholder='Description...' />
+                <Input title="Description" id='discription-input' className='create-group-inputs' type="text" placeholder='Give the members a bit more information...' />
             </div>
             <div className='img-group-container'>
                 <ImageUpload SetImage={SetImage}/>
